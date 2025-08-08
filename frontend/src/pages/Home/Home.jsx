450311e26f568d03a00c761ac6ff49f639f20f7e
Arnav <arnavpriyadrshi2004@gmail.com>
2025-08-08 17:21:29 +0530

    third commit

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import './Home.css'
 import Header from "../../component/Header/Header";
-import ExploreMenu from "../../component/ExploreMenu/ExploreMenu";
-import FoodDisplay from "../../component/FoodDisplay/FoodDisplay";
-import AppDownload from "../../component/appDownload/appDownload";
+import ExploreMenu from "../../component/ExploreMenu/ExploreMenu.jsx";
+import FoodDisplay from "../../component/FoodDisplay/FoodDisplay.jsx";
+import AppDownload from "../../component/appDownload/AppDownload.jsx";
  const Home = () =>{
     const[category, setcategory] = useState("All");
     return(
