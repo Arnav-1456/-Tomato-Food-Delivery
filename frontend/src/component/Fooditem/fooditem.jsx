9c9a96517ecec947601325f27a92dccc1df88eb4
Arnav <arnavpriyadrshi2004@gmail.com>
2025-08-09 02:00:11 +0530

    five commit

--- frontend/src/component/Fooditem/fooditem.jsx
+++ frontend/src/component/Fooditem/fooditem.jsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from "react";
-import './foodItem.css'
+import React, { useContext,  } from "react";
+import './fooditem.css'
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/storeContext";
 const FoodItem = ({id , name , price , description, image}) => {
